5246d849ee82fe57f4827c51066e469e6ec67809
nspalkar <neta.palkar@gmail.com>
2026-09-20 11:03:48 +0530

    Update MyCalc.java

--- src/main/java/mypkg/MyCalc.java
+++ src/main/java/mypkg/MyCalc.java
@@ -19,7 +19,7 @@ public class MyCalc {
 	}
 	public void show()
 	{
-		System.out.println("Hello Jenkins from SL 1 NOV BATCH! Thanks GitHub Action Thanks Subghsish");
+		System.out.println("Hello Jenkins from SL 1 NOV BATCH! Thanks GitHub Action Thanks Netaji");
 	}
 	public void greeting()
 	{
